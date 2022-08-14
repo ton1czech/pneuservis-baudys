1672b0a65d62c4abad62ef244000437ea1c304f6
ton1czech <tonyasek007@gmail.com>
2022-08-14 18:35:50 +0200

    🎁 NEW: basic settings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#1B262C',
+        light: '#BBE1FA',
+        accent: '#0F4C75',
+        accent2: '#3282B8',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
