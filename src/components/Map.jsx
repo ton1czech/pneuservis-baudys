61ccb935d177c0e73a9b6752e2de74207859b8fb
ton1czech <tonyasek007@gmail.com>
2022-08-14 19:04:56 +0200

    🎁 NEW: map

---
+++ src/components/Map.jsx
@@ -0,0 +1,15 @@
+export default function Map() {
+  return (
+    <div>
+      <iframe
+        width='600'
+        height='500'
+        src='https://maps.google.com/maps?q=h%C3%A1jek%2010%20v%C5%A1eruby&t=&z=11&ie=UTF8&iwloc=&output=embed'
+        frameborder='0'
+        scrolling='no'
+        marginheight='0'
+        marginwidth='0'
+      ></iframe>
+    </div>
+  )
+}
