97916cebad87759a85254c9826460d2569f7e1e4
ton1czech <tonyasek007@gmail.com>
2022-08-16 22:38:04 +0200

    👌 IMPROVE: map styles

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,10 @@
 export default function Map() {
   return (
-    <div>
+    <div className='flex justify-end'>
       <iframe
-        width='600'
-        height='500'
         src='https://maps.google.com/maps?q=h%C3%A1jek%2010%20v%C5%A1eruby&t=&z=11&ie=UTF8&iwloc=&output=embed'
-        frameborder='0'
         scrolling='no'
-        marginheight='0'
-        marginwidth='0'
+        className='max-w-[600px] max-h-[600px] w-full aspect-square'
       ></iframe>
     </div>
   )
