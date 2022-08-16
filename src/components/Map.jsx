fa15c26191f5343caedbf25bf62ca65d538bfe2f
ton1czech <tonyasek007@gmail.com>
2022-08-16 23:15:26 +0200

    👌 IMPROVE: darker map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,11 @@
 export default function Map() {
   return (
-    <div className='flex justify-end'>
+    <div className='flex justify-end relative'>
+      <div className='absolute w-full h-full bg-black opacity-[12%]' />
       <iframe
         src='https://maps.google.com/maps?q=h%C3%A1jek%2010%20v%C5%A1eruby&t=&z=11&ie=UTF8&iwloc=&output=embed'
         scrolling='no'
-        className='max-w-[600px] max-h-[600px] w-full aspect-square'
+        className='max-h-[600px] w-full aspect-square'
       ></iframe>
     </div>
   )
