f372b089cf4498212b6c9bcd8e5612f45ef0ea77
ton1czech <tonyasek007@gmail.com>
2022-08-16 23:05:57 +0200

    🎁 NEW: services layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Services from './components/Services'
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <div className='md:max-w-[60%] m-auto'>
         <Navbar />
         <Hero />
+        <Services />
       </div>
     </div>
   )
---
+++ src/components/Services.jsx
@@ -0,0 +1,25 @@
+import tire1 from '../assets/tire-1.jpg'
+import tire2 from '../assets/tire-2.jpg'
+import tire3 from '../assets/tire-3.jpg'
+
+export default function Services() {
+  return (
+    <div className='pb-24' id='services'>
+      <h1 className='heading'>Co nabízíme?</h1>
+      <div className='grid'>
+        <div className='grid grid-cols-2'>
+          <h2 className='text text-center'>přezouvání kol</h2>
+          <img src={tire1} alt='' />
+        </div>
+        <div className='grid grid-cols-2'>
+          <img src={tire2} alt='' />
+          <h2 className='text text-center'>výměna kol</h2>
+        </div>
+        <div className='grid grid-cols-2'>
+          <h2 className='text text-center'>prodej ojetých pneumatik</h2>
+          <img src={tire3} alt='' />
+        </div>
+      </div>
+    </div>
+  )
+}
