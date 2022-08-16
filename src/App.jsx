9bfb25f3a2291dfdcb3df4fc770089cf3a84a33b
ton1czech <tonyasek007@gmail.com>
2022-08-16 22:37:27 +0200

    👌 IMPROVE: mobile padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <div className='bg-dark w-full overflow-hidden font-poppins'>
+    <div className='bg-dark w-full overflow-hidden font-poppins px-5'>
       <div className='md:max-w-[60%] m-auto'>
         <Navbar />
+        <Hero />
       </div>
     </div>
   )
