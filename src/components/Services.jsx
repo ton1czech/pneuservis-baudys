1c712749a3ae65f220c32e56a3ab54b18540e22b
ton1czech <tonyasek007@gmail.com>
2022-08-16 23:06:52 +0200

    👌 IMPROVE: mobile layout services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,15 +7,15 @@ export default function Services() {
     <div className='pb-24' id='services'>
       <h1 className='heading'>Co nabízíme?</h1>
       <div className='grid'>
-        <div className='grid grid-cols-2'>
+        <div className='grid md:grid-cols-2'>
           <h2 className='text text-center'>přezouvání kol</h2>
           <img src={tire1} alt='' />
         </div>
-        <div className='grid grid-cols-2'>
+        <div className='flex flex-col-reverse md:grid md:grid-cols-2'>
           <img src={tire2} alt='' />
           <h2 className='text text-center'>výměna kol</h2>
         </div>
-        <div className='grid grid-cols-2'>
+        <div className='grid md:grid-cols-2'>
           <h2 className='text text-center'>prodej ojetých pneumatik</h2>
           <img src={tire3} alt='' />
         </div>
