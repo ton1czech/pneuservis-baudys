55d13e7b7e415349c675cc6c9cb88eb52a8999c5
ton1czech <tonyasek007@gmail.com>
2022-08-17 00:13:10 +0200

    🎁 NEW: navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,24 @@
+import { GiFlatTire } from 'react-icons/gi'
+
 export default function Navbar() {
   return (
     <nav className='flex justify-between items-center py-6'>
-      <h1 className='heading uppercase'>Pneuservis Baudyš</h1>
-      <ul className='list-none flex gap-5 items-center link'>
-        <li>
+      <h1 className='logo flex items-center content-center gap-2 md:gap-5'>
+        <span className='flex items-center content-center'>
+          <GiFlatTire className='md:w-12 md:h-12 md:pb-1' />
+          servis
+        </span>{' '}
+        baudyš
+      </h1>
+      <ul className='list-none flex gap-3 md:gap-5 items-center'>
+        <li className='link'>
           <a href='#info'>Info</a>
         </li>
-        <li>
+        <li className='link'>
           <a href='#services'>Služby</a>
         </li>
-        <li>
-          <a href='#kontakt'>Kontakt</a>
+        <li className='link'>
+          <a href='#contact'>Kontakt</a>
         </li>
       </ul>
     </nav>
