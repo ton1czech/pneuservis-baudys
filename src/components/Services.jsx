ed177f505aff939064a4c389f5b0ebf7d3caf68d
ton1czech <tonyasek007@gmail.com>
2022-08-17 00:12:35 +0200

    👌 IMPROVE: reusable component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,23 +2,27 @@ import tire1 from '../assets/tire-1.jpg'
 import tire2 from '../assets/tire-2.jpg'
 import tire3 from '../assets/tire-3.jpg'
 
+const Item = ({ image, text, inverted }) =>
+  inverted ? (
+    <div className='grid md:grid-cols-2'>
+      <h2 className='text text-center'>{text}</h2>
+      <img src={image} alt='' />
+    </div>
+  ) : (
+    <div className='flex flex-col-reverse md:grid md:grid-cols-2'>
+      <img src={image} alt='' />
+      <h2 className='text text-center'>{text}</h2>
+    </div>
+  )
+
 export default function Services() {
   return (
     <div className='pb-24' id='services'>
       <h1 className='heading'>Co nabízíme?</h1>
       <div className='grid'>
-        <div className='grid md:grid-cols-2'>
-          <h2 className='text text-center'>přezouvání kol</h2>
-          <img src={tire1} alt='' />
-        </div>
-        <div className='flex flex-col-reverse md:grid md:grid-cols-2'>
-          <img src={tire2} alt='' />
-          <h2 className='text text-center'>výměna kol</h2>
-        </div>
-        <div className='grid md:grid-cols-2'>
-          <h2 className='text text-center'>prodej ojetých pneumatik</h2>
-          <img src={tire3} alt='' />
-        </div>
+        <Item image={tire1} text='přezouvání kol' />
+        <Item image={tire2} text='výměna kol' inverted />
+        <Item image={tire3} text='prodej ojetých pneumatik' />
       </div>
     </div>
   )
