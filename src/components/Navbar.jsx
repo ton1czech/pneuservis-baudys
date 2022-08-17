f4b59c894067335f97826741da7204cb05d0add5
ton1czech <tonyasek007@gmail.com>
2022-08-17 21:59:22 +0200

    👌 IMPROVE: logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,10 @@ import { GiFlatTire } from 'react-icons/gi'
 export default function Navbar() {
   return (
     <nav className='flex justify-between items-center py-6'>
-      <h1 className='logo flex items-center content-center gap-2 sm:gap-3'>
-        <span className='flex items-center content-center'>
-          <GiFlatTire className='sm:pb-[2px]' />
-          servis
-        </span>{' '}
-        baudyš
-      </h1>
+      <div className='logo flex gap-2 sm:gap-3'>
+        <GiFlatTire className='-mr-2 sm:-mr-3 w-4 h-4 sm:w-8 sm:h-8' />
+        servis baudyš
+      </div>
       <ul className='list-none flex gap-3 sm:gap-4 items-center'>
         <li className='link'>
           <a href='#info'>Info</a>
