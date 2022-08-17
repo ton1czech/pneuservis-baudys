01b55f6e5c0fafd08b69af63738df0282fc141e0
ton1czech <tonyasek007@gmail.com>
2022-08-17 23:16:58 +0200

    👌 IMPROVE: logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <Container dark>
       <nav className='flex justify-between items-center py-6'>
         <div className='logo flex gap-2 sm:gap-3'>
-          <GiFlatTire className='-mr-2 sm:-mr-3 w-4 h-4 sm:w-8 sm:h-8' />
+          <GiFlatTire className='-mr-2 sm:-mr-3 w-4 h-4 sm:w-6 sm:h-6' />
           servis baudyš
         </div>
         <ul className='list-none flex gap-3 sm:gap-4 items-center'>
