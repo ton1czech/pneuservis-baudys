89d06ec6aad3b86f192d7e9deb7452db45b2f666
ton1czech <tonyasek007@gmail.com>
2022-08-17 23:05:19 +0200

    👌 IMPROVE: map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,13 @@
 export default function Map() {
   return (
     <div className='flex justify-end relative'>
-      <div className='absolute w-full h-full bg-black opacity-[12%]' />
+      <div className='absolute w-full h-full bg-black opacity-[12%] pointer-events-none' />
       <iframe
-        src='https://maps.google.com/maps?q=h%C3%A1jek%2010%20v%C5%A1eruby&t=&z=11&ie=UTF8&iwloc=&output=embed'
-        scrolling='no'
+        src='https://maps.google.com/maps?q=pneuservis%20baudy%C5%A1&t=&z=11&ie=UTF8&iwloc=&output=embed'
         className='max-h-[600px] w-full aspect-square rounded'
+        scrolling='no'
+        referrerpolicy='no-referrer-when-downgrade'
+        loading='lazy'
       ></iframe>
     </div>
   )
