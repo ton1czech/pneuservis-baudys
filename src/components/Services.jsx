8fd84fa38771d24404d8813f5e068571adfa9800
ton1czech <tonyasek007@gmail.com>
2022-08-19 19:39:53 +0200

    🎁 NEW: service n5

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import tire1 from '../assets/tire-1.jpg'
 import tire2 from '../assets/tire-2.jpg'
 import tire3 from '../assets/tire-3.jpg'
 import tire4 from '../assets/tire-4.jpg'
+import tire5 from '../assets/tire-5.jpg'
 import Container from './Container'
 import { Fade } from 'react-reveal'
 
@@ -38,28 +39,34 @@ export default function Services() {
         <div className='grid'>
           <Item
             image={tire1}
-            subheading='přezouvání kol'
+            subheading='Přezouvání kol'
             text='But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth'
             padding
           />
           <Item
             image={tire2}
-            subheading='výměna kol'
+            subheading='Výměna kol'
             text='At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
             inverted
             padding
           />
           <Item
             image={tire3}
-            subheading='prodej ojetých pneumatik'
+            subheading='Prodej ojetých pneumatik'
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+            padding
+          />
+          <Item
+            image={tire5}
+            subheading='Prodej použitých disků'
+            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+            inverted
             padding
           />
           <Item
             image={tire4}
             subheading='TPMS instalace a diagnostika'
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-            inverted
           />
         </div>
       </div>
