9a7a052c952f425dccdc14de39627ca6931c02fe
ton1czech <tonyasek007@gmail.com>
2022-08-19 22:22:59 +0200

    👌 IMPROVE: small fixes

--- src/components/OpeningHours.jsx
+++ src/components/OpeningHours.jsx
@@ -28,7 +28,7 @@ export default function OpeningHours() {
     <Container dark>
       <div className='py-12 sm:py-24' id='openinghours'>
         <Fade>
-          <div className='py-10 sm:py-16 bg-light rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
+          <div className='p-10 sm:p-16 bg-light rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
             <Item day='Pondělí' />
             <Item day='Úterý' />
             <Item day='Středa' />
--- src/components/Pricelist.jsx
+++ src/components/Pricelist.jsx
@@ -3,8 +3,8 @@ import { Fade } from 'react-reveal'
 
 const Item = ({ price, text }) => (
   <div className='grid grid-cols-[2fr_1fr] gap-20'>
-    <h2 className='subheading'>{text}</h2>
-    <p className='subheading text-2xl'>{price} Kč</p>
+    <h2 className='subheading sm:text-3xl text-base'>{text}</h2>
+    <p className='subheading sm:text-2xl text-base'>{price} Kč</p>
   </div>
 )
 
@@ -13,9 +13,9 @@ export default function Pricelist() {
     <Container>
       <div id='pricelist' className='py-12 sm:py-24'>
         <Fade>
-          <div className='py-10 sm:py-16 bg-dark rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
+          <div className='p-10 sm:p-16 bg-dark rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
             <Item price='500' text='přezutí a vyvážení (4ks)' />
-            <Item price='300-600' text='pneumatika' />
+            <Item price='300' text='pneumatika' />
             <Item price='500' text='chytré ventýlky (4ks)' />
             <Item price='500' text='diagnostika ventýlků' />
           </div>
