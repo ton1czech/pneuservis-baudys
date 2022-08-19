6c79084380339758c50097a4ec45205f663a227e
ton1czech <tonyasek007@gmail.com>
2022-08-19 21:49:08 +0200

    🎁 NEW: price list

--- src/components/Pricelist.jsx
+++ src/components/Pricelist.jsx
@@ -1,10 +1,25 @@
 import Container from './Container'
+import { Fade } from 'react-reveal'
+
+const Item = ({ price, text }) => (
+  <div className='grid grid-cols-[2fr_1fr] gap-20'>
+    <h2 className='subheading'>{text}</h2>
+    <p className='subheading text-2xl'>{price} Kč</p>
+  </div>
+)
 
 export default function Pricelist() {
   return (
     <Container>
       <div id='pricelist' className='py-12 sm:py-24'>
-        <h1 className='darkheading'>Ceník</h1>
+        <Fade>
+          <div className='py-10 sm:py-16 bg-dark rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
+            <Item price='500' text='přezutí a vyvážení (4ks)' />
+            <Item price='300-600' text='pneumatika' />
+            <Item price='500' text='chytrá ventýlky (4ks)' />
+            <Item price='500' text='diagnostika ventýlků' />
+          </div>
+        </Fade>
       </div>
     </Container>
   )
