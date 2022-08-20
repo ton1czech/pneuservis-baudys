47efedf35c9b9821630af82ae3dfcbf96a7c950f
ton1czech <tonyasek007@gmail.com>
2022-08-21 00:19:04 +0200

    👌 IMPROVE: services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,13 +6,20 @@ import tire5 from '../assets/tire-5.jpg'
 import Container from './Container'
 import { Fade } from 'react-reveal'
 
-const Item = ({ image, subheading, text, inverted }) =>
+const Item = ({ image, subheading, text, inverted, banner, buy }) =>
   inverted ? (
     <Fade left>
       <div className='grid sm:grid-cols-2 content-center items-center'>
         <div>
-          <h2 className='darksubheading text-center'>{subheading}</h2>
+          <h2 className='darksubheading text-2xl sm:text-3xl text-left sm:text-center mb-2'>
+            {subheading}
+          </h2>
           <p className='darktext sm:pr-4'>{text}</p>
+          {banner && (
+            <p className='px-4 py-1 mt-3 font-semibold text-2xl rounded text-dark bg-accent w-min'>
+              Novinka!
+            </p>
+          )}
         </div>
         <img src={image} alt={subheading} />
       </div>
@@ -22,8 +29,15 @@ const Item = ({ image, subheading, text, inverted }) =>
       <div className='flex flex-col-reverse sm:grid sm:grid-cols-2 content-center items-center'>
         <img src={image} alt={subheading} />
         <div>
-          <h2 className='darksubheading text-center'>{subheading}</h2>
+          <h2 className='darksubheading text-2xl sm:text-3xl text-left sm:text-center mb-2'>
+            {subheading}
+          </h2>
           <p className='darktext sm:pl-4'>{text}</p>
+          {banner && (
+            <p className='px-4 py-1 sm:ml-4 my-3 sm:mb-0 font-semibold text-2xl rounded text-dark bg-accent w-min'>
+              Novinka!
+            </p>
+          )}
         </div>
       </div>
     </Fade>
@@ -35,30 +49,31 @@ export default function Services() {
       <div className='py-12 sm:py-24' id='services'>
         <div className='grid space-y-20 sm:space-y-0'>
           <Item
+            image={tire4}
+            subheading='TPMS instalace a diagnostika'
+            text='Ventilky a diagnostika od značky Milwaukee pro zjištění tlaku v pneumatice, teploty pneumatiky nebo zjištění točivého momentu'
+            banner
+          />
+          <Item
             image={tire1}
-            subheading='Přezouvání kol'
+            subheading='Přezutí kol'
             text='But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth'
+            inverted
           />
           <Item
             image={tire2}
-            subheading='Výměna kol'
+            subheading='Montáž kol'
             text='At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
-            inverted
           />
           <Item
             image={tire3}
             subheading='Prodej ojetých pneumatik'
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+            inverted
           />
           <Item
             image={tire5}
             subheading='Prodej použitých disků'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-            inverted
-          />
-          <Item
-            image={tire4}
-            subheading='TPMS instalace a diagnostika'
             text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
           />
         </div>
