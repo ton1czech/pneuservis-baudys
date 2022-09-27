2dbf67b7ea7c11a29bd9b29ba2d7ccf2697f341b
ton1czech <tonyasek007@gmail.com>
2022-09-27 20:56:40 +0200

    👌 IMPROVE: hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
         <Fade left>
           <div className='flex flex-col justify-between'>
             <h1 className='heading leading-snug mb-6 sm:mb-0 pr-4'>
-              Veškeré služby pro Vaše auto na jednom místě
+              Veškeré služby pro Vaše kola na jednom místě
             </h1>
             <Fade bottom>
               <div className='hidden lg:flex flex-col space-y-6'>
