9f36726e0fe2f46abc49b71d541f33b83b0ded11
ton1czech <tonyasek007@gmail.com>
2022-09-27 20:55:57 +0200

    🎁 NEW: services cleanup

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,10 +13,9 @@ const Item = ({ image, subheading, text, inverted, banner, buy }) =>
     <Fade left>
       <div className='grid sm:grid-cols-2 content-center items-center'>
         <div className='relative sm:pr-4'>
-          <h2 className='darksubheading text-2xl sm:text-4xl text-left mb-2'>
+          <h2 className='darksubheading text-2xl sm:text-4xl text-center mb-2'>
             {subheading}
           </h2>
-          <p className='darktext text-justify'>{text}</p>
           {banner && (
             <p className='px-4 py-1 mt-3 font-semibold text-2xl rounded text-dark tracking-widest bg-accent w-min'>
               Novinka!
@@ -33,10 +32,9 @@ const Item = ({ image, subheading, text, inverted, banner, buy }) =>
       <div className='flex flex-col-reverse sm:grid sm:grid-cols-2 content-center items-center'>
         <img src={image} alt={subheading} />
         <div className='relative sm:pl-4'>
-          <h2 className='darksubheading text-2xl sm:text-4xl text-left mb-2'>
+          <h2 className='darksubheading text-2xl sm:text-4xl text-center mb-2'>
             {subheading}
           </h2>
-          <p className='darktext text-justify'>{text}</p>
           {banner && (
             <p className='flex items-center gap-1 px-4 py-1 my-3 sm:mb-0 font-semibold text-2xl rounded text-dark tracking-widest bg-accent w-min'>
               {<AiFillTag />}
@@ -57,31 +55,12 @@ export default function Services() {
           <Item
             image={tire4}
             subheading='TPMS instalace a diagnostika'
-            text='Ventilky a diagnostika od značky Milwaukee pro zjištění tlaku v pneumatice, teploty pneumatiky nebo zjištění točivého momentu'
             banner
           />
-          <Item
-            image={tire1}
-            subheading='Přezutí kol'
-            text='But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth'
-            inverted
-          />
-          <Item
-            image={tire2}
-            subheading='Montáž kol'
-            text='At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui.'
-          />
-          <Item
-            image={tire3}
-            subheading='Prodej ojetých pneumatik'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-            inverted
-          />
-          <Item
-            image={tire5}
-            subheading='Prodej použitých disků'
-            text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-          />
+          <Item image={tire1} subheading='Přezutí kol' inverted />
+          <Item image={tire2} subheading='Montáž kol' />
+          <Item image={tire3} subheading='Prodej ojetých pneumatik' inverted />
+          <Item image={tire5} subheading='Prodej použitých disků' />
         </div>
         <img
           className='hidden 2xl:block absolute top-4 left-4'
