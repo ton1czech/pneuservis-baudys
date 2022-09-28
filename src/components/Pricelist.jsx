d79536e4c17250a6ad74dd0c091b8a8233975f2c
ton1czech <tonyasek007@gmail.com>
2022-09-28 20:49:57 +0200

    👌 IMPROVE: prices

--- src/components/Pricelist.jsx
+++ src/components/Pricelist.jsx
@@ -6,7 +6,7 @@ const Item = ({ price, text }) => (
   <>
     <div className='grid grid-cols-[2fr_1fr] gap-12 sm:gap-20'>
       <h2 className='subheading font-normal sm:text-3xl text-base'>{text}</h2>
-      <p className='subheading sm:text-2xl text-base'>{price} Kč</p>
+      <p className='subheading sm:text-2xl text-base'>{price}</p>
     </div>
     <div className='h-px bg-light w-full opacity-50' />
   </>
@@ -18,10 +18,10 @@ export default function Pricelist() {
       <div id='pricelist' className='py-12 sm:py-24'>
         <Fade>
           <div className='p-10 sm:p-16 bg-dark rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
-            <Item price='500' text='přezutí a vyvážení (4ks)' />
-            <Item price='300' text='pneumatika' />
-            <Item price='500' text='chytré ventilky (4ks)' />
-            <Item price='500' text='diagnostika ventilků' />
+            <Item price='od 500 Kč' text='Přezutí a vyvážení (4ks)' />
+            <Item price='domluva' text='Pneumatika' />
+            <Item price='1200 kč' text='Chytré ventilky (1ks)' />
+            <Item price='500 kč' text='Diagnostika ventilků' />
             <GiMoneyStack className='block text-light opacity-5 absolute left-0 top-0 w-full h-full pointer-events-none' />
           </div>
         </Fade>
