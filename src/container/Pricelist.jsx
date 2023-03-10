3cc887111b8b772ec057d1be86b55b310ee5bcec
ton1czech <tonyasek007@gmail.com>
2023-03-10 19:02:50 +0100

    👌 IMPROVE: pricelist refactor

--- src/container/Pricelist.jsx
+++ src/container/Pricelist.jsx
@@ -6,8 +6,10 @@ import { GiMoneyStack } from 'react-icons/gi'
 const Item = ({ price, text }) => (
   <>
     <div className='grid grid-cols-[2fr_1fr] gap-12 sm:gap-20'>
-      <h2 className='subheading font-normal sm:text-2xl text-base'>{text}</h2>
-      <p className='subheading sm:text-2xl text-base'>{price}</p>
+      <h2 className='sm:text-xl text-base text-light'>{text}</h2>
+      <p className='font-semibold sm:text-2xl text-lg text-light text-right'>
+        {price}
+      </p>
     </div>
     <div className='h-px bg-light w-full opacity-50' />
   </>
@@ -20,7 +22,7 @@ export default function Pricelist() {
         <Fade>
           <div className='p-10 sm:p-16 bg-dark rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
             <Item price='od 500 Kč' text='Přezutí a vyvážení (4ks)' />
-            <Item price='domluva' text='Pneumatika' />
+            <Item price='400-1000 Kč' text='Pneumatika' />
             <Item price='1200 kč' text='Chytré ventilky (1ks)' />
             <Item price='500 kč' text='Diagnostika ventilků' />
             <GiMoneyStack className='block text-light opacity-5 absolute left-0 top-0 w-full h-full pointer-events-none' />
