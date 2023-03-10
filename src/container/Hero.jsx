5c5b38426f34bc547839fa3129c5acc4017e24fd
ton1czech <tonyasek007@gmail.com>
2023-03-10 18:55:10 +0100

    👌 IMPROVE: code refactor and prepare for 3d models

--- src/container/Hero.jsx
+++ src/container/Hero.jsx
@@ -4,43 +4,28 @@ import Map from '../components/Map'
 import { Fade } from 'react-reveal'
 import { TbArrowLoopLeft2 } from 'react-icons/tb'
 
-const Card = ({ text }) => (
-  <div className='bg-light rounded-md w-min h-min'>
-    <h1 className='darksubheading text-2xl uppercase p-4 tracking-widest'>
-      {text}
-    </h1>
-  </div>
-)
-
 export default function Hero() {
   return (
     <Container dark>
       <div
-        className='sm:grid sm:grid-cols-2 flex flex-col w-full pt-6 pb-12 sm:pb-24'
+        className='sm:grid sm:grid-cols-2 gap-2 flex flex-col w-full pt-6 pb-12'
         id='home'
       >
         <Fade left>
           <div className='flex flex-col justify-between'>
-            <h1 className='heading leading-snug mb-6 sm:mb-0 pr-4'>
+            <h1 className='text-light text-2xl sm:text-5xl font-semibold mb-6 sm:mb-0 pr-4'>
               Veškeré služby pro Vaše kola na jednom místě
             </h1>
-            <Fade bottom>
-              <div className='hidden lg:flex flex-col space-y-3'>
-                <Card text='levné' />
-                <Card text='rychlé' />
-                <Card text='spolehlivé' />
-              </div>
-            </Fade>
           </div>
         </Fade>
         <Fade right>
-          <div className='relative'>
+          <div>
             <Map />
-            <div className='hidden 2xl:block absolute top-44 -right-72'>
-              <TbArrowLoopLeft2 className='text-light text-[8rem]' />
+            <div className='hidden 2xl:flex mt-6 justify-center'>
               <p className='text-light text-2xl font-semibold'>
                 Najdi si k nám cestu!
               </p>
+              <TbArrowLoopLeft2 className='text-light text-4xl rotate-90 ml-2' />
             </div>
           </div>
         </Fade>
