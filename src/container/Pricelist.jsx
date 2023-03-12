533ccdcdf292c5ea350c342422c2e39c9c155617
ton1czech <tonyasek007@gmail.com>
2023-03-12 22:49:38 +0100

    👌IMPROVE: wider pricelist to be more user friendly on narrow devices

--- src/container/Pricelist.jsx
+++ src/container/Pricelist.jsx
@@ -3,7 +3,7 @@ import Container from '../components/Container'
 import { Fade } from 'react-reveal'
 import { GiMoneyStack } from 'react-icons/gi'
 
-const Item = ({ price, text }) => (
+const Item = ({ price, text, last }) => (
   <>
     <div className='grid grid-cols-[2fr_1fr] gap-8 sm:gap-20'>
       <h2 className='sm:text-xl text-[15px] text-light'>{text}</h2>
@@ -11,7 +11,7 @@ const Item = ({ price, text }) => (
         {price}
       </p>
     </div>
-    <div className='h-px bg-light w-full opacity-50' />
+    {!last && <div className='h-px bg-light w-full opacity-50' />}
   </>
 )
 
@@ -20,11 +20,11 @@ export default function Pricelist() {
     <Container>
       <div id='pricelist' className='py-12 sm:py-24'>
         <Fade>
-          <div className='p-10 sm:p-16 bg-dark rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
+          <div className='py-6 px-4 sm:p-16 bg-dark rounded-lg relative grid justify-center space-y-4 sm:space-y-6'>
             <Item price='500+ Kč' text='Přezutí a vyvážení (4ks)' />
             <Item price='400+ Kč' text='Pneumatika' />
             <Item price='1200 kč' text='Chytré ventilky (1ks)' />
-            <Item price='500 kč' text='Diagnostika ventilků' />
+            <Item price='500 kč' text='Diagnostika ventilků' last />
             <GiMoneyStack className='block text-light opacity-5 absolute left-0 top-0 w-full h-full pointer-events-none' />
           </div>
         </Fade>
